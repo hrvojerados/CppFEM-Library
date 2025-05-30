d7499380226d97a4b60c497e33772504d85aedc0
hrvoje <hr54899@fer.hr>
2025-05-30 22:54:16 +0200

    added Vectors

--- src/tools/math.cpp
+++ src/tools/math.cpp
@@ -1,15 +1,12 @@
 #include <bits/stdc++.h>
-#include <cstdint>
-#include <cstdio>
-#include <stdexcept>
-#include <sys/types.h>
-#include <unordered_map>
 #include <boost/container/small_vector.hpp>
+#include <cstddef>
 using namespace std;
 using ll = long long;
 using u = unsigned;
 using ull = unsigned long long;
 using ld = long double;
+
 
 class Mesh2D {
 public:
@@ -17,10 +14,109 @@ public:
   ll NumberOfElements;
   unordered_map<ll, tuple<double, double>> getNodeCoordinates;
 };
-class Mesh3D {};
-//CSR format matrix
+
+
+class Mesh3D {
+  //One day...
+};
+
+
+template<u size>
+class Vector {
+public:
+  ld* V;
+
+  Vector() {
+    V = new ld[size];
+  }
+
+  Vector(const Vector& other) {
+    V = new ld[size];
+    for (u i = 0; i < size; i++) {
+      V[i] = other.V[i];
+    }
+  }
+
+  Vector(Vector&& other) noexcept {
+    V = other.V;
+    other.V = nullptr;
+  }
+
+  Vector<size>& operator=(const Vector& other) {
+    if (this == &other)
+      return *this;
+    delete[] V;
+    V = new ld[size];
+    for (u i = 0; i < size; i++) {
+      V[i] = other[i];
+    }
+    return *this;
+  }
+
+  Vector& operator=(Vector&& other) noexcept {
+    if (this != &other) {
+      delete[] V;
+      V = other.V;
+      other.V = nullptr;
+    }
+    return *this;
+  }
+
+  ~Vector() {
+    delete[] V;
+  }
+
+  Vector<size> operator+(const Vector& other) const {
+    Vector<size>  result;
+    for (u i = 0; i < size; i++) {
+      result.V[i] = V[i] + other.V[i];
+    } 
+    return result;
+  }
+
+  Vector<size> operator*(const ld scalar) const {
+    Vector<size>  result;
+    for (u i = 0; i < size; i++) {
+      result.V[i] = V[i] * scalar;
+    }
+    return result;
+  }
+
+  const ld& operator[](u i) const {
+    return V[i];
+  }
+
+  ld& operator[](u i) {
+    return V[i];
+  }
+
+  void print() {
+    printf("v = (");
+    for (u i = 0; i < size; i++) {
+      printf("%Lf, ", V[i]);
+    }
+    printf(")\n");
+  }
+
+};
+
+template<u size>
+ld dot(const Vector<size>& V1, const Vector<size>& V2) {
+  ld result = 0;
+  for (u i = 0; i < size; i++) {
+    result += V1[i] * V2[i];
+  }
+  return result;
+}
+
+template<u size>
+Vector<size> operator*(const ld scalar, const Vector<size>& vec) {
+  return vec * scalar;  
+}
+
 template<u meshBranchFactor>
 class SparseMatrix {
+//CSR format matrix
 public:
   ull numOfElements;
   ull numOfRows;
@@ -87,7 +183,15 @@ public:
     if (C[it] == j) {
       A[it] = val;
     }
+  }
 
+  inline void inc(ull i, ull j, ld val) {
+    //for now linear search
+    ull it = R[i];
+    while (C[it] < j) it++;
+    if (C[it] == j) {
+      A[it] += val;
+    }
   }
 
   inline void print() {
@@ -117,12 +221,13 @@ public:
     }
   }
 
-  ~SparseMatrix() {
+  ~SparseMatrix() {//TODO no rule of 5
     delete[] A;
     delete[] C;
     delete[] R;
   }
 };
+
 
 int main() {
   unordered_map<ull, boost::container::small_vector<ull, 6>> neighbours = {
@@ -138,4 +243,15 @@ int main() {
   A->set(0, 2, 3.14);
   A->print();
   printf("%Lf\n", A->get(0, 1));
+  Vector<3> v;
+  v[1] = 1.2;
+  v[2] = 3.14;
+  v[3] = 2.71;
+  Vector<3> u = (2 * v);
+
+  Vector<3> x = u + v;
+  x.print();
+  v.print();
+  u.print();
+  
 }
